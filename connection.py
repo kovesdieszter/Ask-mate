dadad21417f49957b6e114ebf20a628f1fee2b02
DianaDob <dobsinszki.diana@gmail.com>
2021-11-15 15:21:10 +0100

    Add csv dictreader function

--- connection.py
+++ connection.py
@@ -1,8 +1,18 @@
-
+import csv
 #  Bea
 #  Bea
 
 #  Dia
+
+
+def read_from_csv(csv_title):
+    lines_to_read = []
+    with open(csv_title, newline='') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            lines_to_read.append(row)
+    return lines_to_read  # a list of the lines(dictionaries) read out from the csv file
+
 #  Dia
 
 #  Eniko
